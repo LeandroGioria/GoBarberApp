969c978e400ec52b84dcbe4173610b3a6532da30
Leandro Gioria <leandro.gioria@Leandros-MacBook-Air.local>
2019-10-26 16:09:29 -0300

    created list schedule operation

--- src/routes.js
+++ src/routes.js
@@ -7,6 +7,7 @@ import multerConfig from './config/multer';
 import FileController from './app/controllers/FileController';
 import ProviderController from './app/controllers/ProviderController';
 import AppointmentController from './app/controllers/AppointmentController';
+import ScheduleController from './app/controllers/ScheduleController';
 
 const routes = new Router();
 const upload = multer(multerConfig);
@@ -18,9 +19,14 @@ routes.post('/sessions', SessionController.store);
 routes.use(authMiddleware);
 
 routes.put('/users', UserController.update);
+
 routes.get('/providers', ProviderController.index);
+
 routes.post('/files', upload.single('file'), FileController.store);
+
 routes.post('/appointments', AppointmentController.store);
 routes.get('/appointments', AppointmentController.index);
+
+routes.get('/schedule', ScheduleController.index);
 
 export default routes;
