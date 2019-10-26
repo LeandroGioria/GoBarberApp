76cf261916db637f5cc9289e5b3117f841fcb0b2
Leandro Gioria <leandro.gioria@Leandros-MacBook-Air.local>
2019-10-25 23:00:38 -0300

    Created files database, updated Users table, created file controller

--- src/database/index.js
+++ src/database/index.js
@@ -1,8 +1,9 @@
 import Sequelize from 'sequelize';
 import databaseConfig from '../config/database';
 import user from '../app/models/User';
+import file from '../app/models/File';
 
-const models = [user];
+const models = [user, file];
 
 class Database {
     constructor() {
@@ -12,7 +13,12 @@ class Database {
     init() {
         this.connection = new Sequelize(databaseConfig);
 
-        models.map(model => model.init(this.connection));
+        models
+            .map(model => model.init(this.connection))
+            .map(
+                model =>
+                    model.associate && model.associate(this.connection.models)
+            );
     }
 }
 
